2e048ccf5c6f4d22d0e4ca5aedd759bd1b389663
erfa_zhang <64622520+zhangerfa@users.noreply.github.com>
2022-04-19 08:44:12 +0800

    添加周期边界条件时的车辆生成方法

--- vehicle_generator.py
+++ vehicle_generator.py
@@ -5,25 +5,72 @@ from vehicle_enum import Direction
 
 # 车辆生成器
 class Vehicle_generator:
-    '''
+    """
     @:param is_two_way 标记道路上是否包含两个方向行驶车辆,当is_two_way为True时，两侧分别生成向左向右的车辆
     @:param vehicle_type_percent {type ： percent}
-    '''
+    @:param vehicle_num 周期边界条件时，车辆总数
+    当不传入 vehicle_type_percent_dict 和 lam时，则为周期边界条件，一次性生成所有车辆
+    """
 
-    def __init__(self, vehicle_type_percent_dict, lam, is_two_way=False):
+    def __init__(self, vehicle_type_percent_dict, lam=None, vehicle_num=0,
+                 is_two_way=False):
         self.lam = lam  # 泊松分布 λ 参数 dt时间内平均到达车辆数
         self.is_two_way = is_two_way
         self.vehicle_count = 1  # 生成交通参与者计数器
-        # 生成各类交通参与者的概率区间 如（0，0.1）生成car （0.1，1）生成truck
-        self.range_type = {}
+        self.vehicle_num = vehicle_num  # 空间占有率
+        self.vehicle_space = 0  # 车辆占据空间总长度（目前只在周期边界条件下统计）
         percent_sum = 0
-        for vehicle_type in vehicle_type_percent_dict:
-            percent = vehicle_type_percent_dict[vehicle_type]
-            percent_sum += percent
-            self.range_type[percent_sum] = vehicle_type
-            # 当某种车百分百出现 其他车不会生成
-            if percent == 1:
-                break
+        if lam is not None:
+            # 生成各类交通参与者的概率区间 如（0，0.1）生成car （0.1，1）生成truck
+            self.range_type = {}
+            for vehicle_type, percent in vehicle_type_percent_dict.items():
+                percent_sum += percent
+                self.range_type[percent_sum] = vehicle_type
+                # 当某种车百分百出现 其他车不会生成
+                if percent == 1:
+                    break
+        else:
+            self.type_num_dict = {}
+            for vehicle_type, percent in vehicle_type_percent_dict.items():
+                self.type_num_dict[vehicle_type] = round(percent * self.vehicle_num)
+
+    def __update_vehicle_space(self, vehicle):
+        self.vehicle_space += vehicle.length
+
+    # 等间距生成周期边界条件时道路所有车辆，以车辆对象的list返回
+    def init_all_vehicles(self, road):
+        if self.lam is not None:
+            print('传入λ，则执行开口条件，不能调用此方法')
+            return
+
+        lane_num = road.lane_num
+        road_length = road.length
+
+        new_vehicles = []  # 存储所有新生成的车辆
+
+        lane = 0
+        x = 0
+        gap = (lane_num * road_length - self.vehicle_num * self.get_max_vehicle_length()) // self.vehicle_num
+        for vehicle_type, num in self.type_num_dict.items():
+            for i in range(num):
+                new_vehicle = vehicle_type(self.vehicle_count, lane, x, road)
+                self.vehicle_count += 1
+                new_vehicles.append(new_vehicle)
+
+                lane += 1
+                if lane == lane_num:
+                    x = x + gap + self.get_max_vehicle_length()
+                    lane = 0
+
+        return new_vehicles
+
+    # 获取传入车辆中最大车长度
+    def get_max_vehicle_length(self):
+        res = 0  # 最大车长
+        for vehicle_type in self.type_num_dict:
+            tmp = vehicle_type(-1, -1, -1, None)
+            res = max(res, tmp.length)
+        return res
 
     # 返回新生成的交通参与者 如果无法生成返回None
     '''
@@ -66,12 +113,12 @@ class Vehicle_generator:
 
 # 传入车道 有 3 个空元胞时 则认为有供新到达车辆生成的空间
 def has_room(lane, space, direction):
-    '''
+    """
+    :param direction: 生成车辆方向 为 Direction枚举类对象
+    :param space:
     :param lane: 车道index
-    :param index_space: 空间矩阵（矩形）：无对象：0 有对象：对象索引
-    @:param direction: 生成车辆方向 为 Direction枚举类对象
     :return:
-    '''
+    """
     road_length = len(space[0])
     x_range = range(3) if direction == Direction.right else range(road_length - 3, road_length)
     if direction == Direction.left:
