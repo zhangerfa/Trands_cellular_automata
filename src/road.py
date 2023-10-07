1bb122a8e5773317472ba46de7e6682b55743b38
201 <877319363@qq.com>
2023-10-07 19:45:22 +0800

    增加查询车辆所在道路类型的方法

--- src/road.py
+++ src/road.py
@@ -257,13 +257,24 @@ class Road:
 
 
 # 车道数据记录类
+# 一个车道可由多种类型的道路组成
+# 提供查询车辆所在位置的道路类型的方法
 class Lane:
     def __init__(self, section_length_dict):
         self.section_length_dict = section_length_dict
-        self.end_x_section_dict = {}  # {某段末尾位置 ： 类型}
+        self.end_x_section_dict = {}  # {末尾位置 ： 类型}
         self.length = 0  # 车道长度
         self.section_length_dict = section_length_dict
         # 更新end_x_section_dict和车道长度
         for vehicle_type in section_length_dict:
             self.length += section_length_dict[vehicle_type]
             self.end_x_section_dict[self.length] = vehicle_type
+
+    # 查询车辆所在位置的道路类型
+    def which_section(self, vehicle):
+        if vehicle.x > self.length:
+            print('在使用Lane.which_section()时，车辆位置超出车道长度')
+            return None
+        for end_x in self.end_x_section_dict:
+            if vehicle.x <= end_x:
+                return self.end_x_section_dict[end_x]
